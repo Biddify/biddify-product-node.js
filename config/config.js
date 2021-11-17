5c0e6881359865c842021677e7fbb0efe7e9519b
Rob Rutjes <450901@student.fontys.nl>
2021-11-17 20:49:28 +0100

    Updated config.js

--- config/config.js
+++ config/config.js
@@ -9,12 +9,12 @@ module.exports = {
     dialect : process.env.DB_DIALECT
   },
   test: {
-    username: process.env.DB_TEST_USER,
-    password: process.env.DB_TEST_PASSWORD,
-    database: process.env.DB_TEST_DATABASE,
-    host    : process.env.DB_TEST_HOST,
-    port    : process.env.DB_TEST_PORT,
-    dialect : process.env.DB_TEST_DIALECT
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_TEST,
+    host    : process.env.DB_HOST,
+    port    : process.env.DB_PORT,
+    dialect : process.env.DB_DIALECT
   },
   production: {
     "username": null,
