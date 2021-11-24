d5578487923510893e70a1d48cbfccb7d49d3b46
Rob Rutjes <450901@student.fontys.nl>
2021-11-24 23:31:05 +0100

    Updated tests

--- test/product.test.js
+++ test/product.test.js
@@ -1,8 +1,8 @@
 const app = require('../server');
 const request = require('supertest');
 
-describe('Post Endpoints', () => {
-    it('Should create a new product', async () => {
+describe('US-04 | Als verkoper, wil ik een product kunnen aanbieden, zodat ik deze kan verkopen', () => {
+    it('Correct body POST request', async () => {
         const res = await request(app)
             .post('/api/products')
             .send({
@@ -10,7 +10,46 @@ describe('Post Endpoints', () => {
                 "description": "Test omschrijving"
             })
         expect(res.statusCode).toEqual(201)
+
         expect(res.body).toHaveProperty('id')
+        expect(res.body).toHaveProperty('title')
+        expect(res.body).toHaveProperty('description')
+        expect(res.body).toHaveProperty('updatedAt')
+        expect(res.body).toHaveProperty('createdAt')
+    })
+
+    it('Incorrect body (without title) POST request', async () => {
+        const res = await request(app)
+            .post('/api/products')
+            .send({
+                "description": "Test omschrijving"
+            })
+        expect(res.statusCode).toEqual(400)
+    })
+
+    it('Incorrect body (without description) POST request', async () => {
+        const res = await request(app)
+            .post('/api/products')
+            .send({
+                "title": "Test",
+            })
+        expect(res.statusCode).toEqual(400)
+    })
+
+    it('Incorrect empty body POST request', async () => {
+        const res = await request(app)
+            .post('/api/products')
+        expect(res.statusCode).toEqual(400)
+    })
+
+    it('Incorrect URL POST request', async () => {
+        const res = await request(app)
+            .post('/api/product')
+            .send({
+                "title": "Test",
+                "description": "Test omschrijving"
+            })
+        expect(res.statusCode).toEqual(404)
     })
 })
 
