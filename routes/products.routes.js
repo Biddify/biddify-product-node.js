a85ac65afa680f9618b6499c08117d3a59b2318e
Rob Rutjes <450901@student.fontys.nl>
2022-01-12 09:28:18 +0100

    Added swagger api documentation

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,9 +1,8 @@
+const products = require('../controllers/product.controller')
 module.exports = app => {
   const products = require('../controllers/product.controller.js')
 
   const router = require('express').Router()
-
-  router.post('/', products.create)
 
   /**
    * @swagger
@@ -11,15 +10,120 @@ module.exports = app => {
    *  get:
    *    description: Use to request all products
    *    responses:
-   *      '200':
+   *      '201':
    *        description: A successful response
    */
 
   router.get('/', products.findAll)
 
+  /**
+   * @swagger
+   * /products/{id}:
+   *  get:
+   *    description: Use to request one product
+   *    consumes:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        description: Product ID
+   *        required: true
+   *        schema:
+   *          type: integer
+   *          format: int64
+   *    responses:
+   *      '201':
+   *        description: A successful response
+   */
+
   router.get('/:id', products.findOne)
+  
+  /**
+   * @swagger
+   * /products:
+   *  post:
+   *    description: Use to create a product
+   *    consumes:
+   *      - application/json
+   *    parameters:
+   *      - in: body
+   *        name: product
+   *        description: The product to create.
+   *        schema:
+   *          type: object
+   *          properties:
+   *            title:
+   *              type: string
+   *            description:
+   *              type: string
+   *    responses:
+   *      '201':
+   *        description: A successful response
+   */
+
+  router.post('/', products.create)
+
+  /**
+   * @swagger
+   * /products/{id}:
+   *  put:
+   *    description: Use to update a product
+   *    consumes:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        description: Product ID
+   *        required: true
+   *        schema:
+   *          type: integer
+   *          format: int64
+   *      - in: body
+   *        name: product
+   *        description: The product to create.
+   *        schema:
+   *          type: object
+   *          properties:
+   *            title:
+   *              type: string
+   *            description:
+   *              type: string
+   *    responses:
+   *      '201':
+   *        description: A successful response
+   */
 
   router.put('/:id', products.update)
+
+  /**
+   * @swagger
+   * /products/{id}:
+   *  put:
+   *    description: Use to update a product
+   *    consumes:
+   *      - application/json
+   *    parameters:
+   *      - name: id
+   *        in: path
+   *        description: Product ID
+   *        required: true
+   *        schema:
+   *          type: integer
+   *          format: int64
+   *      - in: body
+   *        name: product
+   *        description: The product to create.
+   *        schema:
+   *          type: object
+   *          properties:
+   *            title:
+   *              type: string
+   *            description:
+   *              type: string
+   *    responses:
+   *      '201':
+   *        description: A successful response
+   */
 
   router.delete('/:id', products.delete)
 
